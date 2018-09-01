c344a124d4484f6709fa4566fce2d04d4de990dd
e <yi_curious@foxmail.com>
2018-09-01 11:48:25 +0800

    change dir

--- easyLabel/easyLabel/settings.py
+++ easyLabel/easyLabel/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'tag',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/files/'
+MEDIA_URL = 'media/'
---
+++ easyLabel/tag/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Label1(models.Model):
+    """
+    可能有多个标签类型，比如发型、脸型之类，所以此处标号为1
+    """
+    label1 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.label
+
+
+class Picture(models.Model):
+    """
+    存储图片基本信息
+    """
+    name = models.CharField(max_length=255)
+    origin = models.CharField(max_length=255)  # 图片从何而来
+    size = models.IntegerField()
+    image = models.ImageField(upload_to='images')
+    label1 = models.ForeignKey(Label1, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return "picture {} size:{}*{} at {}".format(
+            self.name, self.size, self.size, self.image)
+
+    def get_absolute_url(self):
+        return 'picture_detail', None, {'object_id':self.id}
+
+
