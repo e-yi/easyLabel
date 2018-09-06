766310fa1e99fd12f01e8385972ba531db90a307
e-yi <e_yi@foxmail.com>
2018-09-06 21:13:41 +0800

    补充api

--- easyLabel/tag/urls.py
+++ easyLabel/tag/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('pictures/',
+         views.picture_info),
     path('pictures/list/',
          views.picture_list),
     path('pictures/<int:pk>',
