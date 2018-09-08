0cc9c81fa8ce14ea97e43a66ac1714f458163c71
e <yi_curious@foxmail.com>
2018-09-08 22:53:22 +0800

    collect static

--- easyLabel/easyLabel/settings.py
+++ easyLabel/easyLabel/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#MEDIA_ROOT = '/files/'
-#MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_common')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_common')
